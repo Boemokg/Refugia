135776436ea797eec9a8f03782a914fe67381db1
Kagiso Nhlapo <130784383+Boemokg@users.noreply.github.com>
2025-02-10 18:32:52 +0200

    Delete hydrorefugia-main directory

--- hydrorefugia-main/Code/Old scripts/getpentadsfromAPI.R
+++
@@ -1,5 +0,0 @@
-library(sf)
-
-qds <- st_read("https://api.birdmap.africa/sabap2/v2/pentads/project/sabap2?format=geoJSON")
-
-st_write(qds, "/home/jasper/Documents/Datasets/QDS/qds_pentads.geojson")
